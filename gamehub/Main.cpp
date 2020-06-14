3ad9285161f9e0581f3128f19cc5bd02affef7d0
Athos van Kralingen <athos.van.kralingen@hva.nl>
2020-06-14 21:53:11 +0200

    Fixed uninitialized rolls bug, fixed some other small bugs, added info about winning attacker/defender, added stats for amount of wins/losses

--- gamehub/Main.cpp
+++ gamehub/Main.cpp
@@ -14,6 +14,7 @@ template<unsigned TSize>
 std::array<unsigned, TSize> RollDice(std::array<Die, TSize>& a_Dice, unsigned a_NumRolls)
 {
 	std::array<unsigned, TSize> rolls;
+	rolls.fill(0);
 	for (unsigned i = 0; i < a_NumRolls; i++)
 	{
 		rolls[i] = a_Dice[i].Roll();
@@ -24,15 +25,15 @@ std::array<unsigned, TSize> RollDice(std::array<Die, TSize>& a_Dice, unsigned a_
 
 SimulationResult Simulate(std::array<Die, 2>& a_DefenderDice, std::array<Die, 3>& a_AttackerDice)
 {
-	unsigned defendingUnits = 50000;
-	unsigned attackingUnits = 50000;
-	while (defendingUnits > 0 && attackingUnits > 0)
+	unsigned defendingUnits = 15;
+	unsigned attackingUnits = 15;
+	while (defendingUnits > 0 && attackingUnits > 1)
 	{
-		unsigned numAttackDice = std::min(3u, attackingUnits);
+		unsigned numAttackDice = std::min(3u, attackingUnits - 1);
 		std::array<unsigned, 3> attackResults = RollDice(a_AttackerDice, numAttackDice);
 		unsigned numLargeRolls = std::count_if(attackResults.begin(), attackResults.end(), [](unsigned roll) { return roll > 3; });
 
-		unsigned numDefenderDice = numLargeRolls > 1 ? 1 : std::min(defendingUnits, 2u);
+		unsigned numDefenderDice =  std::min(defendingUnits, numLargeRolls > 1 && defendingUnits > 2u ? 1u : 2u);
 		std::array<unsigned, 2> defenderResults = RollDice(a_DefenderDice, numDefenderDice);
 
 		for (unsigned i = 0; i < std::min(numDefenderDice, numAttackDice); i++)
@@ -55,10 +56,23 @@ int main()
 	std::array<Die, 2> defenderDice;
 	std::array<Die, 3> attackerDice;
 
+	unsigned attackWins = 0;
+	unsigned defenseWins = 0;
 	while (true)
 	{
 		SimulationResult simResult = Simulate(defenderDice, attackerDice);
-		std::cout << "Attackers left: " << simResult.attackersLeft << "\nDefenders left: " << simResult.defendersLeft << "\n";
+		if (simResult.attackersLeft > simResult.defendersLeft)
+		{
+			attackWins++;
+			std::cout << "Attackers won";
+		}
+		else
+		{
+			defenseWins++;
+			std::cout << "Defenders won";
+		}
+		std::cout << "\nAttackers left: " << simResult.attackersLeft << "\nDefenders left: " << simResult.defendersLeft << "\n";
+		std::cout << "Attackers have won " << attackWins << " out of " << (attackWins + defenseWins);
 		char c;
 		std::cin >> c;
 	}
